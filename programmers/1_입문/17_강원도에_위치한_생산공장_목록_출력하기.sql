b024bfd8bc2a3e95ebda944df020a7b76c56575f
owerq8 <xilicon@naver.com>
2026-01-21 17:44:45 +0900

    [프로그래머스] 입문 - 강원도에 위치한 생산공장 목록 출력하기 (#17)

---
+++ programmers/1_입문/17_강원도에_위치한_생산공장_목록_출력하기.sql
@@ -0,0 +1,13 @@
+-- 강원도에 위치한 생산공장 목록 출력하기
+-- 프로그래머스 입문 (⭐)
+-- 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/131112
+-- 작성자: 김규리
+-- 작성일: 2026. 01. 21. 17:44:43
+
+-- 코드를 입력하세요
+SELECT FACTORY_ID,
+       FACTORY_NAME,
+       ADDRESS
+FROM FOOD_FACTORY
+WHERE ADDRESS LIKE ("%강원도%")
+ORDER BY FACTORY_ID ASC;
