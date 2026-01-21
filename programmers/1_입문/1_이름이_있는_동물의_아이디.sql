5e2beb69583e4975666e8eae8a9deb66a2c94ebf
owerq8 <xilicon@naver.com>
2026-01-21 16:47:23 +0900

    [프로그래머스] 입문 - 이름이 있는 동물의 아이디 (#1)

---
+++ programmers/1_입문/1_이름이_있는_동물의_아이디.sql
@@ -0,0 +1,10 @@
+-- 이름이 있는 동물의 아이디
+-- 프로그래머스 입문 (⭐)
+-- 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/59407
+-- 작성자: 김규리
+-- 작성일: 2026. 01. 21. 16:47:21
+
+-- 코드를 입력하세요
+SELECT ANIMAL_ID
+FROM ANIMAL_INS
+WHERE NAME IS NOT NULL
